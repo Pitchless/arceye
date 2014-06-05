afdfa95bbf7b7e1d5c60a4526fb1f1ca3a0bbc9a
daveindustries <info@davidwilman.com>
2014-06-05 11:49:11 +0100

    Update arduino_md03_v_5_0.ino
    Added a debug option to remove the LCD updates and speed up the main loop

--- arduino/arduino_md03_v_5_0/arduino_md03_v_5_0.ino
+++ arduino/arduino_md03_v_5_0/arduino_md03_v_5_0.ino
@@ -20,6 +20,7 @@
  LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 */
 
+const int debug = 0;
 const int potPin1 = A0;
 const int potPin2 = A1;
 const int potPin3 = A2;
@@ -81,30 +82,27 @@ boolean stringComplete = false;  // whether the string is complete
 String statusString = "";        // a string to hold the output
 
 void setup(){
-  /*
-  Wire.begin(); //start I2C bus
-  delay(100); //wait for bus to stabalise
-  */
+  if (debug == 1){
+     Wire.begin(); //start I2C bus
+     delay(100); //wait for bus to stabalise
+     lcd.begin (16,2); // 2 rows x 16 char
+     // Switch on the backlight
+     lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+     lcd.setBacklight(HIGH);
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print("ArcEyes MD03 I2C");
+     lcd.setCursor(0,1);
+     lcd.print("SandBox");
+     delay(30);
+     //lcd.clear();
+  }
   pinMode(rlyPin1,OUTPUT); // Setup pins
   pinMode(rlyPin2,OUTPUT); // Setup pins
   pinMode(rlyPin3,OUTPUT); // Setup pins
   pinMode(motorPinDir1,OUTPUT); // Setup pins
   pinMode(motorPinDir2,OUTPUT); // Setup pins
   pinMode(motorPinDir3,OUTPUT); // Setup pins
-  
-  /*
-  lcd.begin (16,2); // 2 rows x 16 char
-  // Switch on the backlight
-  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-  lcd.setBacklight(HIGH);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("ArcEyes MD03 I2C");
-  lcd.setCursor(0,1);
-  lcd.print("SandBox");
-  delay(30);
-  //lcd.clear();
-  */
   
   //serial
   Serial.begin(115200);
@@ -197,16 +195,17 @@ void read_command() {
 }
 
 // Debug: Show current commands on lcd
-*/
 void lcd_command() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(
-          "Y"+String(cmd.yaw_pwm)+" "+String(cmd.yaw_direction)
-          +" L"+String(cmd.lid_pwm)+" "+String(cmd.lid_direction)
-          );
-}
-*/
+  if (debug == 1){
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print(
+             "Y"+String(cmd.yaw_pwm)+" "+String(cmd.yaw_direction)
+             +" L"+String(cmd.lid_pwm)+" "+String(cmd.lid_direction)
+             );
+        }
+} 
+
 
 // Send command to the motors
 void do_command() {
@@ -231,56 +230,13 @@ void send_status() {
   Serial.println(statusString);
 }
 
-byte manualSpeedControl(int speedPin, byte potPin, int rlyPin, int dirPin, byte direct, int speedControl){
-    int potVal = analogRead(potPin);
-    if(potVal < 450){               //Sets reverse speed from Pot1
-      digitalWrite(rlyPin, LOW);
-      speedControl = map(potVal, 450, 0, 0, 250);
-      direct = LOW;
-    }
-    else if(potVal < 573){          // sets zero speed from Pot1
-      digitalWrite(rlyPin, HIGH);
-      speedControl = 0;
-    }
-    else if (potVal >= 574){        // sets forward speed from Pot1
-      digitalWrite(rlyPin, LOW);
-      speedControl = map(potVal, 574, 1023, 0, 250);
-      direct = HIGH;
-    }
-    analogWrite(speedPin, speedControl);  
-    digitalWrite(dirPin, direct);          
- 
-    return direct;
-}
-/*
-byte getData(int address, byte reg){                   // function for getting data from MD03
-  Wire.beginTransmission(address);
-  Wire.write(reg);
-  Wire.endTransmission();
-  
-  byte data = 0;
-  //for (int i; i<100; i++){
-    Wire.requestFrom(address, 1);         // Requests byte from MD03
-    while(Wire.available() < 1);          // Waits for byte to become availble
-    data = Wire.read();
-    //}
-  
-  return(data);
-}
-
-void sendData(int address, byte reg, byte val){         // Function for sending data to MD03
-  Wire.beginTransmission(address);         // Send data to MD03
-    Wire.write(reg);
-    Wire.write(val);
-  Wire.endTransmission();
-}
-*/
 /*
   SerialEvent occurs whenever a new data comes in the
  hardware serial RX.  This routine is run between each
  time loop() runs, so using delay inside loop can delay
  response.  Multiple bytes of data may be available.
  */
+ 
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
@@ -296,8 +252,9 @@ void serialEvent() {
       stringComplete = true;
     } 
   }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(inputString);
-
+  if (debug == 1){
+     lcd.clear();
+     lcd.setCursor(0,0);
+     lcd.print(inputString);
+  }
 }
